05ade77ede9011a5909e2e2be5416e51294bd9fd
ShadowLp174 <samuel.moravec@yahoo.com>
2022-07-14 19:22:10 +0200

    init

---
+++ spec/libre-copilot-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LibreCopilotAtomView from '../lib/libre-copilot-atom-view';
+
+describe('LibreCopilotAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
